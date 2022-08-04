eb224cfbf82179d89f4fc263cec7f1974a8a96f2
bebaocute1010 <xduc24122002@gmail.com>
2022-08-04 11:41:01 +0700

    Fixed size and update show-on-map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.slider').slick({
         autoplay: true,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 2000,
         arrows: false,
     });
 });
@@ -30,3 +30,26 @@ $(document).ready(function () {
         </span></button>`,
     });
 });
+
+
+// SHOW-ON-MAP
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var show_on_map = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+show_on_map.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
