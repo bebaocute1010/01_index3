bae8807bbddc2b01a4d66feb1716f4fec914e239
bebaocute1010 <xduc24122002@gmail.com>
2022-08-04 17:17:30 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,52 +4,55 @@ $(document).ready(function () {
         autoplaySpeed: 2000,
         arrows: false,
     });
-});
 
-$(document).ready(function () {
     $('.deal-of-weak-content').slick({
         slidesToShow: 5,
         draggable: false,
-        prevArrow:`<button type='button' class='slick-prev stick-arrows'><span class="material-symbols-outlined">
+        prevArrow: `<button type='button' class='slick-prev stick-arrows'><span class="material-symbols-outlined">
         arrow_back_ios
         </span></button>`,
-        nextArrow:`<button type='button' class='slick-next stick-arrows'><span class="material-symbols-outlined">
+        nextArrow: `<button type='button' class='slick-next stick-arrows'><span class="material-symbols-outlined">
         arrow_forward_ios
         </span></button>`,
     });
-});
-$(document).ready(function () {
+
     $('.recommend-grid-product').slick({
         slidesToShow: 5,
         draggable: false,
-        prevArrow:`<button type='button' class='slick-prev stick-arrows'><span class="material-symbols-outlined">
+        prevArrow: `<button type='button' class='slick-prev stick-arrows'><span class="material-symbols-outlined">
         arrow_back_ios
         </span></button>`,
-        nextArrow:`<button type='button' class='slick-next stick-arrows'><span class="material-symbols-outlined">
+        nextArrow: `<button type='button' class='slick-next stick-arrows'><span class="material-symbols-outlined">
         arrow_forward_ios
         </span></button>`,
     });
 });
 
 
-// SHOW-ON-MAP
+function makeTimer() {
 
+    var endTime = new Date("29 April 2023 9:56:00 GMT+01:00");
+    endTime = (Date.parse(endTime) / 1000);
 
-// Get the modal
-var modal = document.getElementById("myModal");
+    var now = new Date();
+    now = (Date.parse(now) / 1000);
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var show_on_map = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-show_on_map.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
+    var timeLeft = endTime - now;
+
+    var days = Math.floor(timeLeft / 86400);
+    var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+    var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+    var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+    if (hours < "10") { hours = "0" + hours; }
+    if (minutes < "10") { minutes = "0" + minutes; }
+    if (seconds < "10") { seconds = "0" + seconds; }
+
+    $("#days").html(days);
+    $("#hours").html(hours);
+    $("#mins").html(minutes);
+    $("#secs").html(seconds);
+
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
+setInterval(function () { makeTimer(); }, 1000);
